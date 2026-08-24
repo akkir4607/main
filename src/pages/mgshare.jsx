f4dd70a96766c8e558b34e2536e38407a951eb6f
akkir4607 <145462898+akkir4607@users.noreply.github.com>
2026-08-24 10:14:35 +0530

    Update mgshare.jsx

--- src/pages/mgshare.jsx
+++ src/pages/mgshare.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import './MGShare.css';
+import './mgshare.css';
 
 const MGShare = () => {
   const [transferProgress, setTransferProgress] = useState(0);
